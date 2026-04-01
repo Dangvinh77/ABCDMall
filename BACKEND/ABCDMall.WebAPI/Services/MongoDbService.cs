0df4697433847d1e1373427aefc00bdd98797a9f
Harori <phamdangvinh2002@gmail.com>
2026-04-01 19:22:15 +0700

    Update: connectDB MongoDB

---
+++ BACKEND/ABCDMall.WebAPI/Services/MongoDbService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace ABCDMall.WebAPI.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbService(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+        }
+
+        public IMongoCollection<T> GetCollection<T>()
+        {
+            var collectionName = typeof(T).Name + "s";
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
---
+++ BACKEND/ABCDMall.WebAPI/Services/MongoDbSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace ABCDMall.WebAPI.Services
+{
+    public class MongoDbSetting
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+}
